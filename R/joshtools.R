f3b778013e85be348e336495ebdfaaa79b69ebb0
Joshua Nugent <joshuanugent@gmail.com>
2023-02-17 12:34:46 -0800

    add OR to RR

--- R/joshtools.R
+++ R/joshtools.R
@@ -10,7 +10,11 @@ logit <- function(p){
   qlogis(p)
 }
 
-odds_ratio <- function(p1, p2){
-  (p1 / (1-p1)) / (p2 / (1-p2))
+odds_ratio <- function(baseline_p, p2){
+  # p1 is baseline
+  (p2 / (1-p2)) / (baseline_p / (1-baseline_p))
 }
 
+rr_from_or <- function(or, baseline_p){
+  or / ((1-baseline_p) + (baseline_p * or))
+}
